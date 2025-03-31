b566d3ee003a7cd06e31c33ebd9e007e48cd2f4d
Andrew William Watson <aww@awwsmm.com>
2025-03-30 21:40:46 -0400

    1.4-9

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -101,7 +101,7 @@ if (5 < 10) {
 		{token.INT, "10"},
 		{token.SEMICOLON, ";"},
 		{token.INT, "10"},
-		{token.NOT_EQ, "=="},
+		{token.NOT_EQ, "!="},
 		{token.INT, "9"},
 		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
