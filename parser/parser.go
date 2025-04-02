ce12f52417f9aa5ab2db9da5df7f36fd8008127c
Andrew William Watson <aww@awwsmm.com>
2025-04-01 21:22:52 -0400

    2.5-3

--- parser/parser.go
+++ parser/parser.go
@@ -63,6 +63,8 @@ func (p *Parser) parseStatement() ast.Statement {
 	switch p.curToken.Type {
 	case token.LET:
 		return p.parseLetStatement()
+	case token.RETURN:
+		return p.parseReturnStatement()
 	default:
 		return nil
 	}
