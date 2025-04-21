0cd28885d0f62e684ef37859f70d5581b6d9ac95
Andrew William Watson <aww@awwsmm.com>
2025-04-21 19:46:02 -0400

    4.2-11

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -288,6 +288,20 @@ func evalInfixExpression(
 	}
 }
 
+func evalStringInfixExpression(
+	operator string,
+	left, right object.Object,
+) object.Object {
+	if operator != "+" {
+		return newError("unknown operator: %s %s %s",
+			left.Type(), operator, right.Type())
+	}
+
+	leftVal := left.(*object.String).Value
+	rightVal := right.(*object.String).Value
+	return &object.String{Value: leftVal + rightVal}
+}
+
 func evalIntegerInfixExpression(
 	operator string,
 	left, right object.Object,
