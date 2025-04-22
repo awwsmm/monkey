d40a8acc13f681047bb9d8bbeb7b90dc4057c35f
Andrew William Watson <aww@awwsmm.com>
2025-04-21 20:47:14 -0400

    4.3-4

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -156,12 +156,15 @@ func evalIdentifier(
 	node *ast.Identifier,
 	env *object.Environment,
 ) object.Object {
-	val, ok := env.Get(node.Value)
-	if !ok {
-		return newError("identifier not found: %s", node.Value)
+	if val, ok := env.Get(node.Value); ok {
+		return val
 	}
 
-	return val
+	if builtin, ok := builtins[node.Value]; ok {
+		return builtin
+	}
+
+	return newError("identifier not found: %s", node.Value)
 }
 
 func evalProgram(program *ast.Program, env *object.Environment) object.Object {
