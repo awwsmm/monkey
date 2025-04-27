af3e86bcbb9e0b2885380f9d7ec1d718df5c7f38
Andrew William Watson <aww@awwsmm.com>
2025-04-27 18:02:05 -0400

    4.4-9

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -834,3 +834,26 @@ func TestParsingEmptyArrayLiterals(t *testing.T) {
 		t.Fatalf("len(array.Elements) not 0. got=%d", len(array.Elements))
 	}
 }
+
+func TestParsingIndexExpressions(t *testing.T) {
+	input := "myArray[1 + 1]"
+
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+	indexExp, ok := stmt.Expression.(*ast.IndexExpression)
+	if !ok {
+		t.Fatalf("exp not ast.IndexExpression. got=%T", stmt.Expression)
+	}
+
+	if !testIdentifier(t, indexExp.Left, "myArray") {
+		return
+	}
+
+	if !testInfixExpression(t, indexExp.Index, 1, "+", 1) {
+		return
+	}
+}
