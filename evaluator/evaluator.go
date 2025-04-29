59234a8aee62167ac0e83c8934bd6b4e34225028
Andrew William Watson <aww@awwsmm.com>
2025-04-29 18:42:59 -0400

    4.4-19

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -130,6 +130,18 @@ func evalIndexExpression(left, index object.Object) object.Object {
 	}
 }
 
+func evalArrayIndexExpression(array, index object.Object) object.Object {
+	arrayObject := array.(*object.Array)
+	idx := index.(*object.Integer).Value
+	max := int64(len(arrayObject.Elements) - 1)
+
+	if idx < 0 || idx > max {
+		return NULL
+	}
+
+	return arrayObject.Elements[idx]
+}
+
 func applyFunction(fn object.Object, args []object.Object) object.Object {
 	switch fn := fn.(type) {
 	case *object.Function:
