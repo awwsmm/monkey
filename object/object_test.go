cb6665ec71104f0812344f3cbfddd58724cb2fa2
Andrew William Watson <aww@awwsmm.com>
2025-04-29 23:57:42 -0400

    4.5-10

--- object/object_test.go
+++ object/object_test.go
@@ -20,3 +20,41 @@ func TestStringHashKey(t *testing.T) {
 		t.Errorf("strings with different content have same hash keys")
 	}
 }
+
+func TestBooleanHashKey(t *testing.T) {
+	hello1 := &Boolean{Value: true}
+	hello2 := &Boolean{Value: true}
+	diff1 := &Boolean{Value: false}
+	diff2 := &Boolean{Value: false}
+
+	if hello1.HashKey() != hello2.HashKey() {
+		t.Errorf("booleans with same value have different hash keys")
+	}
+
+	if diff1.HashKey() != diff2.HashKey() {
+		t.Errorf("booleans with same value have different hash keys")
+	}
+
+	if hello1.HashKey() == diff1.HashKey() {
+		t.Errorf("booleans with different values have same hash keys")
+	}
+}
+
+func TestIntegerHashKey(t *testing.T) {
+	hello1 := &Integer{Value: 42}
+	hello2 := &Integer{Value: 42}
+	diff1 := &Integer{Value: 43}
+	diff2 := &Integer{Value: 43}
+
+	if hello1.HashKey() != hello2.HashKey() {
+		t.Errorf("integers with same value have different hash keys")
+	}
+
+	if diff1.HashKey() != diff2.HashKey() {
+		t.Errorf("integers with same value have different hash keys")
+	}
+
+	if hello1.HashKey() == diff1.HashKey() {
+		t.Errorf("integers with different values have same hash keys")
+	}
+}
