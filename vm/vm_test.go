ccc91519aa674b36a4a639eff02b911db08cd15f
Andrew William Watson <aww@awwsmm.com>
2025-05-07 12:13:37 -0400

    2.0-24

--- vm/vm_test.go
+++ vm/vm_test.go
@@ -75,3 +75,13 @@ func testExpectedObject(
 		}
 	}
 }
+
+func TestIntegerArithmetic(t *testing.T) {
+	tests := []vmTestCase{
+		{"1", 1},
+		{"2", 2},
+		{"1 + 2", 2}, //  FIXME
+	}
+
+	runVmTests(t, tests)
+}
