5d8793e85f9fbd45fa4e33756e1a8b77c16034b8
Andrew William Watson <aww@awwsmm.com>
2025-05-17 12:00:00 -0400

    5.0-5

--- compiler/symbol_table.go
+++ compiler/symbol_table.go
@@ -21,3 +21,10 @@ func NewSymbolTable() *SymbolTable {
 	s := make(map[string]Symbol)
 	return &SymbolTable{store: s}
 }
+
+func (s *SymbolTable) Define(name string) Symbol {
+	symbol := Symbol{Name: name, Index: s.numDefinitions, Scope: GlobalScope}
+	s.store[name] = symbol
+	s.numDefinitions++
+	return symbol
+}
