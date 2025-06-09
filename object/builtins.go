62029a85cdaebcb87b46461ccf7c1d0a465b695e
Andrew William Watson <aww@awwsmm.com>
2025-06-08 20:58:26 -0400

    8.0-6

--- object/builtins.go
+++ object/builtins.go
@@ -37,6 +37,27 @@ var Builtins = []struct {
 		},
 		},
 	},
+	{
+		"first",
+		&Builtin{Fn: func(args ...Object) Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got=%d, want=1",
+					len(args))
+			}
+			if args[0].Type() != ARRAY_OBJ {
+				return newError("argument to `first` must be ARRAY, got %s",
+					args[0].Type())
+			}
+
+			arr := args[0].(*Array)
+			if len(arr.Elements) > 0 {
+				return arr.Elements[0]
+			}
+
+			return nil
+		},
+		},
+	},
 }
 
 func newError(format string, a ...interface{}) *Error {
