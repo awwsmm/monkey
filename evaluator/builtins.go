e8a72642181824f7d21097119fa704964b5e50d3
Andrew William Watson <aww@awwsmm.com>
2025-06-08 21:12:37 -0400

    8.0-10

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -5,15 +5,10 @@ import (
 )
 
 var builtins = map[string]*object.Builtin{
-	"len": object.GetBuiltinByName("len"),
-
+	"len":   object.GetBuiltinByName("len"),
 	"first": object.GetBuiltinByName("first"),
-
-	"last": object.GetBuiltinByName("last"),
-
-	"rest": object.GetBuiltinByName("rest"),
-
-	"push": object.GetBuiltinByName("push"),
-
-	"puts": object.GetBuiltinByName("puts"),
+	"last":  object.GetBuiltinByName("last"),
+	"rest":  object.GetBuiltinByName("rest"),
+	"push":  object.GetBuiltinByName("push"),
+	"puts":  object.GetBuiltinByName("puts"),
 }
