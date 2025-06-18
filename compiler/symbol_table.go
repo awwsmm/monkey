55aca86ae6b79cbdd0239bd169677c00d474bcaf
Andrew William Watson <aww@awwsmm.com>
2025-06-17 23:31:02 -0400

    9.1-7

--- compiler/symbol_table.go
+++ compiler/symbol_table.go
@@ -20,11 +20,14 @@ type SymbolTable struct {
 
 	store          map[string]Symbol
 	numDefinitions int
+
+	FreeSymbols []Symbol
 }
 
 func NewSymbolTable() *SymbolTable {
 	s := make(map[string]Symbol)
-	return &SymbolTable{store: s}
+	free := []Symbol{}
+	return &SymbolTable{store: s, FreeSymbols: free}
 }
 
 func NewEnclosedSymbolTable(outer *SymbolTable) *SymbolTable {
