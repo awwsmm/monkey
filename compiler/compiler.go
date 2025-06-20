c5405db6a30176ba01b3266ff4fdb641577a3286
Andrew William Watson <aww@awwsmm.com>
2025-06-19 21:55:11 -0400

    9.3-16

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -448,5 +448,7 @@ func (c *Compiler) loadSymbol(s Symbol) {
 		c.emit(code.OpGetBuiltin, s.Index)
 	case FreeScope:
 		c.emit(code.OpGetFree, s.Index)
+	case FunctionScope:
+		c.emit(code.OpCurrentClosure)
 	}
 }
